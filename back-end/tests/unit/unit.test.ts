98c9e4d0f8f888a434006019e3e2357da39d6d44
Joao Paulo <oliveirajpufrj@gmail.com>
2022-08-02 01:21:49 -0300

    feat: some new unit tests xd

--- back-end/tests/unit/unit.test.ts
+++ back-end/tests/unit/unit.test.ts
@@ -6,9 +6,9 @@ import { recommendationRepository } from "../../src/repositories/recommendationR
 
 
 describe("insert recommendations", () => {
-    it("should create recommendation", async () => {
+    it("create recommendation", async () => {
       const recommendation = {
-        name: faker.lorem.words(4),
+        name: 'You will Be Back',
         youtubeLink: "https://www.youtube.com/watch?v=eKFN-aqPJH8",
       };
       jest
@@ -21,9 +21,9 @@ describe("insert recommendations", () => {
       expect(recommendationRepository.create).toBeCalledTimes(1);
     });
   
-    it("should throw a conflict error if the name of the recommendation is not unique", async () => {
+    it("error if the name of the recommendation is not unique", async () => {
       const recommendation = {
-        name: faker.lorem.words(3),
+        name: 'You will Be Back',
         youtubeLink: "https://www.youtube.com/watch?v=eKFN-aqPJH8",
       };
   
@@ -37,3 +37,80 @@ describe("insert recommendations", () => {
       });
     });
   });
+
+  describe('Up/downvote', () => {
+    it('upvote valid id', async () => {
+      const recommendation = {
+        id: 1,
+        name: "Alexander Hamilton",
+        youtubeLink: "https://www.youtube.com/watch?v=VhinPd5RRJw"
+      };
+  
+      jest
+        .spyOn(recommendationRepository, "find")
+        .mockImplementationOnce((): any => {
+          return {
+            id: recommendation.id,
+            name: recommendation.name,
+            youtubeLink: recommendation.youtubeLink,
+            score: 0
+          };
+        });
+      jest
+        .spyOn(recommendationRepository, "updateScore")
+        .mockImplementationOnce((): any => {});
+  
+      await recommendationService.upvote(recommendation.id);
+      expect(recommendationRepository.find).toBeCalled();
+      expect(recommendationRepository.updateScore).toBeCalled();
+    })
+
+    it('downvote valid id', async () => {
+      const recommendationId = 1;
+      jest
+        .spyOn(recommendationRepository, "find")
+        .mockImplementationOnce((): any => {});
+  
+      const promise = recommendationService.upvote(recommendationId);
+      expect(promise).rejects.toEqual({ type: "not_found", message: "" });
+    });
+  
+    it("Should downvote recommendation", async () => {
+      const recommendation = {
+        id: 1,
+        name: "Alexander Hamilton",
+        youtubeLink: "https://www.youtube.com/watch?v=VhinPd5RRJw"
+      };
+  
+      jest
+        .spyOn(recommendationRepository, "find")
+        .mockImplementationOnce((): any => {
+          return {
+            id: recommendation.id,
+            name: recommendation.name,
+            youtubeLink: recommendation.youtubeLink,
+            score: 0
+          };
+        });
+  
+      jest
+        .spyOn(recommendationRepository, "updateScore")
+        .mockImplementationOnce((): any => {
+          return {
+            id: recommendation.id,
+            name: recommendation.name,
+            youtubeLink: recommendation.youtubeLink,
+            score: -1
+          };
+        });
+  
+      jest
+        .spyOn(recommendationRepository, "remove")
+        .mockImplementationOnce((): any => {});
+  
+      await recommendationService.downvote(recommendation.id);
+      expect(recommendationRepository.find).toBeCalled();
+      expect(recommendationRepository.updateScore).toBeCalled();
+      expect(recommendationRepository.remove).not.toBeCalled();
+    })
+  })
